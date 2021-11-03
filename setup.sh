2e993bcb556a2a486d7d73ff0758c707a5b8d3fb
Sindre Thomassen <sindrtho@hotmail.com>
2021-11-03 14:33:09 +0100

    Dataset setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+for I in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18;
+	do mkdir Video_$I;
+	for J in ambient intensity range;
+		do mkdir Video_$I/$J;
+	done;
+	mv Video000$I* Video_$I;
+done
