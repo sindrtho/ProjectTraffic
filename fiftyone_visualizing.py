f657567c69cbe2e4956c4865ab9aa53071c3b628
janmarius <janmariusv@outlook.com>
2021-12-05 02:56:05 +0100

    WIP: Commit before testing a way to do evaluation

--- fiftyone_visualizing.py
+++ fiftyone_visualizing.py
@@ -1,5 +1,10 @@
 import fiftyone as fo
 
+'''
+    Use this file to visualize the dataset.
+    Source: https://towardsdatascience.com/how-to-work-with-object-detection-datasets-in-coco-format-9bf4fb5848a4
+'''
+
 name = 'my-dataset'
 dataset_dir = './data_fiftyone'
 dataset_type = fo.types.COCODetectionDataset
--- movie_utils.py
+++ movie_utils.py
@@ -3,26 +3,10 @@ import re
 import moviepy.editor as mpy
 
 
-def atoi(text):
-    return int(text) if text.isdigit() else text
-
-
-def natural_keys(text):
-    return [atoi(c) for c in re.split('(\d+)', text)]
-
-
 def export_video_from_frames(path, filename, fps=30):
     frames = preprocess_frames(path)
     clips = mpy.ImageSequenceClip(frames, load_images=True, fps=fps)
     clips.write_videofile(filename, fps=30)
-
-
-def get_files_in_directory(path):
-    result = []
-    for root, _, files in os.walk(path):
-        for file in files:
-            result.append(file)
-    return result
 
 
 def preprocess_frames(path):
@@ -34,3 +18,17 @@ def preprocess_frames(path):
     return frames
 
 
+def get_files_in_directory(path):
+    result = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            result.append(file)
+    return result
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split('(\d+)', text)]
